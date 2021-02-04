e02d245d046c71b8f4f059f6bbbfbdc854435ad4
vanemcb <2970@holbertonschool.com>
2021-02-04 22:46:48 +0000

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints numbers (0 - 10
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		printf("%d", num);
+	}
+	putchar(10);
+return (0);
+}
+
