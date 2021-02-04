15f89e4e2092bf88343011f3432cb28a99043d12
vanemcb <2970@holbertonschool.com>
2021-02-04 21:05:43 +0000

    Task 1 fixed 4

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 
 	if (ld > 5)
 	{
-		printf("Last digit of %i is %i and is greather than 5\n", n, ld);
+		printf("Last digit of %i is %i and is greater than 5\n", n, ld);
 	}
 	else if (ld == 0)
 	{
