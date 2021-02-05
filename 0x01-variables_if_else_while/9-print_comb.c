71acffa3110ba8fd10fd6b420029ea715e206cc8
vanemcb <2970@holbertonschool.com>
2021-02-05 01:56:31 +0000

    Task 9 fixed

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,18 +10,20 @@
 int main(void)
 {
 	int num1;
-	int num2;
 
 	for (num1 = 48; num1 <= 57; num1++)
 	{
-		for (num2 = 48; num2 <= 57; num2++)
-			putchar(num1);
-			putchar(num2);
+		putchar(num1);
+		if (num1 < 57)
+		{
 			putchar(44);
 			putchar(32);
 		}
+		else
+		{
+			putchar(10);
+		}
 	}
-	putchar(10);
 return (0);
 }
 
