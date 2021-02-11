3a1b8c52f74208809e620c70588af51d17e79c41
vanemcb <2970@holbertonschool.com>
2021-02-11 04:15:22 +0000

    Task 16 fixed

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,21 +9,21 @@
 int main(void)
 {
 	int i;
-	unsigned long int fibo, fibo1 = 1, fibo2 = 2;
+	long double fibo, fibo1 = 1, fibo2 = 2;
 
 	printf("%d, %d, ", 1, 2);
-	for (i = 1; i <= 96; i++)
+	for (i = 1; i <= 98; i++)
 	{
 		fibo = fibo1 + fibo2;
 		fibo1 = fibo2;
 		fibo2 = fibo;
-		if (i < 96)
+		if (i < 98)
 		{
-			printf("%lu, ", fibo);
+			printf("%.0Lf, ", fibo);
 		}
 		else
 		{
-			printf("%lu", fibo);
+			printf("%.0Lf", fibo);
 		}
 	}
 	printf("\n");
