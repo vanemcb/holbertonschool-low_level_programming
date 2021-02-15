11712abfccd34c7d51ce235e20da79a24f510038
vanemcb <2970@holbertonschool.com>
2021-02-15 22:40:40 +0000

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
