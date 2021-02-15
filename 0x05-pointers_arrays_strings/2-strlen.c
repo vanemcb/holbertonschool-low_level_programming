cdfad63f570cb568890d518741ac851b415a888c
vanemcb <2970@holbertonschool.com>
2021-02-15 21:44:29 +0000

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: string length
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
