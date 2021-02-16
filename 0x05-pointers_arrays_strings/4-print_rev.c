7c1fb701b26b97000225ddcb85f936cfde9a9bb1
vanemcb <2970@holbertonschool.com>
2021-02-16 13:57:40 +0000

    Task 4 fixed

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,3 +1,5 @@
+#include "holberton.h"
+
 /**
  * print_rev - prints a string, in reverse, followed by a new line
  * @s: string
@@ -5,15 +7,9 @@
 
 void print_rev(char *s)
 {
-	int i = 0, x;
+	int x;
 
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-
-
-	for (x = i; x >= 0; x--)
+	for (x = _strlen(s); x >= 0; x--)
 	{
 		_putchar(s[x]);
 	}
