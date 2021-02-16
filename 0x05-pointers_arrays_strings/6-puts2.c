37d61434f8b565b1e12be01b6210dd6b401512f3
vanemcb <2970@holbertonschool.com>
2021-02-16 21:34:20 +0000

    Task 6 fixed

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -13,9 +13,9 @@ void puts2(char *str)
 	while (str[i] != '\0')
 		i++;
 
-	for (x = 0; x <= i; x = x + 2)
+	for (x = 0; x <= i - 1; x = x + 2)
 	{
-		if (x <= i)
+		if (x <= i - 1)
 			_putchar(str[x]);
 	}
 	_putchar('\n');
