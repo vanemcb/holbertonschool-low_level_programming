0c823a1dde93e4be68ed16a88ddf117d9d0b19ef
vanemcb <2970@holbertonschool.com>
2021-02-18 18:12:50 +0000

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,11 +7,11 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int n = 0, n2 = 0, i, i2;
+	int n = 0, n2 = 0, i, i2 = 0;
 
-	while (dest[n] != '\n')
+	while (dest[n] != '\0')
 		n++;
-	while (dest[n2] != '\n')
+	while (src[n2] != '\0')
 		n2++;
 
 	for (i = n; i <= n + n2; i++)
