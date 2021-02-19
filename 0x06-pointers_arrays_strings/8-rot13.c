c4b8df613c82cd3cc463df0579d44a7c67367c2e
vanemcb <2970@holbertonschool.com>
2021-02-19 03:44:45 +0000

    Task 8

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,25 @@
+/**
+ * rot13 - encodes a string using rot13
+ * @s: input string
+ * Return: s
+ */
+
+char *rot13(char *s)
+{
+	char letter1[] = "ABCDEFGHIJKLMabcdefghijklm";
+	char letter2[] = "NOPQRSTUVWXYZnopqrstuvwxyz";
+	int i = 0, x;
+
+	while (s[i] != '\0')
+	{
+		for (x = 0; x < 26; x++)
+		{
+			if (s[i] == letter1[x])
+				s[i] = s[i] + 13;
+			else if (s[i] == letter2[x])
+				s[i] = s[i] - 13;
+		}
+		i++;
+	}
+	return (s);
+}
