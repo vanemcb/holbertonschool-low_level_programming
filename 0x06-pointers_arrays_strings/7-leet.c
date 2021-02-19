d1c43620bb2cf3b4f2c15c6a3086bb3738323b52
vanemcb <2970@holbertonschool.com>
2021-02-19 01:55:13 +0000

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+/**
+ * leet - encodes a string into 1337
+ * @letter: input character
+ * Return: letter
+ */
+
+char *leet(char *letter)
+{
+	char l[] = {'A', 'E', 'O', 'T', 'L'};
+	char L[] = {'a', 'e', 'o', 't', 'l'};
+	char num[] = {'4', '3', '0', '7', '1'};
+	int i, x;
+
+	while (letter[i] != '\0')
+	{
+		for (x = 0; x < 5; x++)
+		{
+			if (letter[i] == l[x] || letter[i] == L[x])
+				letter[i] = num[x];
+		}
+		i++;
+	}
+	return (letter);
+}
