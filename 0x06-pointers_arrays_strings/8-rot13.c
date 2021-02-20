6a354a01609a2b4f1d7469d9709e88d367104247
vanemcb <2970@holbertonschool.com>
2021-02-20 01:07:00 +0000

    Task 8 fixed

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -6,19 +6,20 @@
 
 char *rot13(char *s)
 {
-	char letter1[] = "ABCDEFGHIJKLMabcdefghijklm";
-	char letter2[] = "NOPQRSTUVWXYZnopqrstuvwxyz";
+	char letter1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLM\
+abcdefghijklmnopqrstuvwxyzabcdefghijklm";
 
 	int i = 0, x = 0;
 
 	while (s[i] != '\0')
 	{
-		for (x = 0; x < 26; x++)
+		for (x = 0; x < 78; x++)
 		{
 			if (s[i] == letter1[x])
-				s[i] = s[i] + 13;
-			else if (s[i] == letter2[x])
-				s[i] = s[i] - 13;
+			{
+				s[i] = letter1[x + 13];
+				break;
+			}
 		}
 		i++;
 	}
