a7766f12571e4fff30a7812ac0a2a8c632b60c7b
vanemcb <2970@holbertonschool.com>
2021-02-20 01:28:15 +0000

    Task 8 fixed

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -6,8 +6,14 @@
 
 char *rot13(char *s)
 {
-	char letter1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLM\
-abcdefghijklmnopqrstuvwxyzabcdefghijklm";
+	char letter1[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+			  'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+			  'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd',
+			  'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'A',
+			  'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+			  'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+			  'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E',
+			  'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
 
 	int i = 0, x = 0;
 
