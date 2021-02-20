4a1e0bd1cb1894ef8006bb09e70dfc5472b63a26
vanemcb <2970@holbertonschool.com>
2021-02-20 05:08:24 +0000

    Task 100 fixed

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -9,21 +9,26 @@ void print_number(int n)
 {
 	int num, i, b = 0, x = 1000000000;
 
-	for (i = 0; i <= 9; i++)
+	if (n == 0)
+		_putchar('0');
+	else
 	{
-		num = n / x;
-		n = n % x;
-
-		if (num != 0)
-			b = 1;
-		if (num < 0 && b == 1)
+		for (i = 0; i <= 9; i++)
 		{
-			num = -num;
-			n = -n;
-			_putchar('-');
+			num = n / x;
+			n = n % x;
+
+			if (num != 0)
+				b = 1;
+			if (num < 0 && b == 1)
+			{
+				num = -num;
+				n = -n;
+				_putchar('-');
+			}
+			if (b == 1)
+				_putchar(num + '0');
+			x = x / 10;
 		}
-		if (b == 1)
-			_putchar(num + '0');
-		x = x / 10;
 	}
 }
