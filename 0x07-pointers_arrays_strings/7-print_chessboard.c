6288119e07e83e957c4769acad218385e03d8aa5
vanemcb <2970@holbertonschool.com>
2021-02-22 16:53:08 +0000

    Task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * print_chessboard - function that prints the chessboard
+ * @a: input board
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, x;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (x = 0; x < 8; x++)
+			_putchar(a[i][x]);
+		_putchar('\n');
+	}
+}
--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -3,3 +3,4 @@ int _putchar(char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
+void print_chessboard(char (*a)[8]);
