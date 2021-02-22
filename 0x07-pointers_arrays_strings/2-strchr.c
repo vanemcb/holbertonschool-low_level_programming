0c1ba33e87f2088d8f20bd3120250f906c7cdf5b
vanemcb <2970@holbertonschool.com>
2021-02-22 21:06:15 +0000

    Task 2 fixed

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 /**
  * _strchr - function that locates a character in a string
  * @s: input string
@@ -9,7 +8,7 @@
 char *_strchr(char *s, char c)
 {
 	int i = 0;
-	char *s2 = NULL;
+	char *s2 = "NULL";
 
 	while (s[i] != '\0')
 	{
