31a16d0dd7ed98f1d38652abdb1427aa099be270
vanemcb <2970@holbertonschool.com>
2021-02-23 21:56:57 +0000

    holberton.h updated

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -4,3 +4,7 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
+char *_strpbrk(char *s, char *accept);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+char *_strstr(char *haystack, char *needle);
