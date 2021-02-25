bb98006afe9c32e40f2a6651e61c5d24b1960b4c
vanemcb <2970@holbertonschool.com>
2021-02-25 22:52:24 +0000

    betty style correction

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,15 +1,14 @@
+#include "holberton.h"
 /**
  * is_prime_number - function that recognizes if a number is prime
  * @n: input number
  * Return: 1 if the number is prime otherwise return 0
  */
 
-int aux_is_prime(int n, int c);
-
 int is_prime_number(int n)
 {
 	int p;
-    
+
 	if (n < 0)
 		return (0);
 	if (n == 1)
@@ -24,7 +23,7 @@ int is_prime_number(int n)
  * aux_is_prime - auxiliar function to recognizes if a number is prime
  * @c: counter
  * @n: input number
- * Return: 1 if the number is prime otherwise return 0 
+ * Return: 1 if the number is prime otherwise return 0
  */
 
 int aux_is_prime(int n, int c)
@@ -34,4 +33,4 @@ int aux_is_prime(int n, int c)
 	else if (n % c == 0 && n != c)
 		return (0);
 	return (aux_is_prime(n, c + 1));
-}   
+}
--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -7,3 +7,4 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int sqrt_value(int c, int n);
 int is_prime_number(int n);
+int aux_is_prime(int n, int c);
