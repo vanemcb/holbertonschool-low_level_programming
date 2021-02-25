a7e2763b79cd6278181938545f6263ccc8ccec7d
vanemcb <2970@holbertonschool.com>
2021-02-25 22:46:47 +0000

    Function is_prime_number created

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,37 @@
+/**
+ * is_prime_number - function that recognizes if a number is prime
+ * @n: input number
+ * Return: 1 if the number is prime otherwise return 0
+ */
+
+int aux_is_prime(int n, int c);
+
+int is_prime_number(int n)
+{
+	int p;
+    
+	if (n < 0)
+		return (0);
+	if (n == 1)
+		return (0);
+	if (n == 0)
+		return (0);
+	p = aux_is_prime(n, 2);
+	return (p);
+}
+
+/**
+ * aux_is_prime - auxiliar function to recognizes if a number is prime
+ * @c: counter
+ * @n: input number
+ * Return: 1 if the number is prime otherwise return 0 
+ */
+
+int aux_is_prime(int n, int c)
+{
+	if (n % c == 0 && n == c)
+		return (1);
+	else if (n % c == 0 && n != c)
+		return (0);
+	return (aux_is_prime(n, c + 1));
+}   
--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -6,3 +6,4 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int sqrt_value(int c, int n);
+int is_prime_number(int n);
