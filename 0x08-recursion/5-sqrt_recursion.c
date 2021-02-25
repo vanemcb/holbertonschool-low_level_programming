b83ae308c99273932cc04075e20599836cf17798
vanemcb <2970@holbertonschool.com>
2021-02-25 04:31:58 +0000

    Function _sqrt_recursion created

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+/**
+ * _sqrt_recursion - function that returns the natural square root of a number
+ * @n: input number
+ * @c: counter
+ * Return: sqrt
+ */
+
+int sqrt_value(int c, int n);
+
+int _sqrt_recursion(int n)
+{
+	int sqrt;
+
+	if (n < 0)
+		return (-1);
+	sqrt = sqrt_value(1, n);
+	return (sqrt);
+}
+
+/**
+ * sqrt_value - evaluate the natural square of the number
+ * @c: counter
+ * @n: input number
+ * Return: natural square root
+ */
+
+int sqrt_value(int c, int n)
+{
+	if (c * c == n)
+		return (c);
+	if (c * c > n)
+		return (-1);
+	return (sqrt_value(c + 1, n));
+}
