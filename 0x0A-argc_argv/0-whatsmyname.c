410a24ef9376af69d079503b821958b1b44343a5
vanemcb <2970@holbertonschool.com>
2021-03-01 03:53:53 +0000

    __attribute__((unused)) added to argument argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,9 +7,8 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	if (argc == 1)
-		printf("%s\n", argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
