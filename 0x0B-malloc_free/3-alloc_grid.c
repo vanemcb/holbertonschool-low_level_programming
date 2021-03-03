59e303d96699bf1be4ea0d35f312f57882073197
vanemcb <2970@holbertonschool.com>
2021-03-03 03:20:00 +0000

    Fuction 3-alloc_grid.c created

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - function that returns a pointer to
+ * a 2 dimensional array of integers
+ * @width: input width
+ * @height: input height
+ * Return: pointer to array
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **array_2d;
+	int i, x;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	array_2d = malloc(sizeof(int *) * height);
+	for (i = 0; i < height; i++)
+		array_2d[i] = malloc(sizeof(int) * width);
+
+	if (array_2d == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		for (x = 0; x < width; x++)
+			array_2d[i][x] = 0;
+	}
+	return (array_2d);
+}
