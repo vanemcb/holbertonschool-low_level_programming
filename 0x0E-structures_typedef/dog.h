e07dc8c042f7b48e993c8d02cfd466ef635da229
vanemcb <2970@holbertonschool.com>
2021-03-08 22:05:33 +0000

    Guard and comments added

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog information
+ * @name: pointer to dog's name'
+ * @age: dog's name
+ * @owner: pointer to dod's owner name
+ */
+
 typedef struct dog
 {
 	char *name;
 	float age;
-	char *owner; 
+	char *owner;
 } dog;
+
+#endif
