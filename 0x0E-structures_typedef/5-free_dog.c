e74265f60e7b1f60bfc5a36b8c77f2536b3e03b1
vanemcb <2970@holbertonschool.com>
2021-03-09 03:54:15 +0000

    Function free_dog created

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - function that frees dogs
+ * @d: input structure
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
