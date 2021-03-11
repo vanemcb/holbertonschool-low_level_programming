d849736fb26d0eafb6c983159b7a770a11395ae1
vanemcb <2970@holbertonschool.com>
2021-03-11 19:38:32 +0000

    Task 1 fixed

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,7 +21,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(nums, int));
-		if (i < n - 1)
+		if (i < n - 1 && separator != NULL)
 			for (x = 0; x < len; x++)
 			printf("%c", separator[x]);
 		else
