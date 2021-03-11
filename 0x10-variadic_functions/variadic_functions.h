2e6eadab97887bfb606b9a7c5c0b019bd844df1b
vanemcb <2970@holbertonschool.com>
2021-03-11 20:04:49 +0000

    Task 2 fixed

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,5 +2,5 @@
 #define VARIADIC_FUNCTIONS_H
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
-int _strlen(char *s);
+void print_strings(const char *separator, const unsigned int n, ...);
 #endif
