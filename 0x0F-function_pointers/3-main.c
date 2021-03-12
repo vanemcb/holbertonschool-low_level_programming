466f79d7b4d3d89359c48f2659eb183764679a4a
vanemcb <2970@holbertonschool.com>
2021-03-12 23:48:41 +0000

    Program calculator created

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,29 +8,32 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	int result;
+	int (*operator)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	/**if (argv[2] != '+' && argv[2] != '-' && argv[2] != '*'	\
-	    && argv[2] != '/' && argv[2] != '%')
+
+	if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == '0')
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	operator = get_op_func(argv[2]);
+
+	if (operator == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((argv[2] == '/' || argv[2] == '%') && argv[3] == 0)
-	{
-		printf("Error\n");
-		exit(100);
-		}
-	*/
 
-	result = (get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
+	result = operator(atoi(argv[1]), atoi(argv[3]));
 
 	printf("%d\n", result);
 
