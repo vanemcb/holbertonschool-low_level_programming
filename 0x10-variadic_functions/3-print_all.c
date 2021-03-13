4e88593bd8df1a70b313261ff0c6ebb819c54d7d
vanemcb <2970@holbertonschool.com>
2021-03-13 21:44:06 +0000

    Function print_all created

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,84 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - function that prints anything
+ * @format: list of types of arguments passed to the function
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list input_args;
+	int i1 = 0, i2 = 0;
+	char *space = "";
+
+	id print_selec[] = {
+		{"c", p_char},
+		{"i", p_int},
+		{"f", p_float},
+		{"s", p_string},
+		{NULL, NULL}
+	};
+
+	va_start(input_args, format);
+
+	while (format[i1] != '\0')
+	{
+		while (print_selec[i2].id_string != NULL)
+		{
+			if (print_selec[i2].id_string[0] == format[i1])
+			{
+				printf("%s", space);
+				print_selec[i2].print_function(input_args);
+				space = ", ";
+			}
+			i2++;
+		}
+		i2 = 0;
+		i1++;
+	}
+	va_end(input_args);
+	printf("\n");
+}
+
+/**
+ * p_char - function that prints a character
+ * @input_text: input text
+ */
+void p_char(va_list input_text)
+{
+	printf("%c", va_arg(input_text, int));
+}
+
+/**
+ * p_int - function that prints an integer
+ * @input_text: input text
+ */
+void p_int(va_list input_text)
+{
+	printf("%d", va_arg(input_text, int));
+}
+
+/**
+ * p_float - function that prints a float
+ * @input_text: input text
+ */
+void p_float(va_list input_text)
+{
+	printf("%f", va_arg(input_text, double));
+}
+
+/**
+ * p_string - function that prints a string
+ * @input_text: input text
+ */
+void p_string(va_list input_text)
+{
+	char *str = va_arg(input_text, char *);
+
+	if (str == NULL)
+		printf("(nil)");
+	printf("%s", str);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,28 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+/**
+ * struct identifier - Struct identifier
+ *
+ * @id_print_string: identifier to type data
+ * @print_function: the function associated to print
+ */
+typedef struct identifier
+{
+	char *id_string;
+	void (*print_function)(va_list);
+} id;
+
+void p_char(va_list input_text);
+void p_int(va_list input_text);
+void p_float(va_list input_text);
+void p_string(va_list input_text);
+
 #endif
