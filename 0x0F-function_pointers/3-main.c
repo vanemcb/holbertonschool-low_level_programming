f17cd67325a15b7cdafe597f6467fe41ce154bb9
vanemcb <2970@holbertonschool.com>
2021-03-13 00:48:26 +0000

    Main program fixed

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,10 +8,9 @@
  * Return: 0
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 	int result;
-	int (*operator)(int, int);
 
 	if (argc != 4)
 	{
@@ -19,21 +18,19 @@ int main(int argc, char **argv)
 		exit(98);
 	}
 
-	if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == '0')
+	if ((argv[2][0] == '/' || *argv[2] == '%') && *argv[3] == '0')
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	operator = get_op_func(argv[2]);
-
-	if (operator == NULL)
+	if (get_op_func(argv[2]) == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	result = operator(atoi(argv[1]), atoi(argv[3]));
+	result = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
 
 	printf("%d\n", result);
 
