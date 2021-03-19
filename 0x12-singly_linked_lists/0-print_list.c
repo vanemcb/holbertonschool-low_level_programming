3f0be567ea68547afe55f1b06405a8d5dec9e7e7
vanemcb <2970@holbertonschool.com>
2021-03-19 14:36:25 +0000

    Function print_list created

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_list - function that prints all the elements of a list
+ * @h: pointer to the first node
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int cont = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%d]", 0);
+			printf("%s\n", "(nil)");
+			h = h->next;
+		}
+		else
+		{
+			printf("[%d] ", h->len);
+			printf("%s\n", h->str);
+			h = h->next;
+		}
+		cont++;
+	}
+	return (cont);
+}
