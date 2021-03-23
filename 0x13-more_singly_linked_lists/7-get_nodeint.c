a1f7a6de3d0263cb2f4b48de74f9c489d546469b
vanemcb <2970@holbertonschool.com>
2021-03-23 20:16:48 +0000

    Return condition added

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,5 +14,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		return (NULL);
 	for (x = 0; x < index; x++)
 		head = head->next;
+	if (!head)
+		return (NULL);
 	return (head);
 }
