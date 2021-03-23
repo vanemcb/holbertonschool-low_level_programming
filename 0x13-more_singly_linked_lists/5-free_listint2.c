240db189d9bda4d96213fa311ecdbcc76c18a55c
vanemcb <2970@holbertonschool.com>
2021-03-23 14:47:47 +0000

    Function free_listint2 created

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a list
+ * @head: pointer to the head of the list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *head_aux = *head;
+
+	while (head_aux->next)
+	{
+		free(head_aux);
+		head_aux = head_aux->next;
+	}
+	free(head_aux);
+	head = NULL;
+}
