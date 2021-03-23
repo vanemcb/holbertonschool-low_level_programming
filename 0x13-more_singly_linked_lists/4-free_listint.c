f90e474e5291142b0e95b2939c6c9d15794f0271
vanemcb <2970@holbertonschool.com>
2021-03-23 03:47:46 +0000

    Function free_listint created

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * free_listint - function that frees a list
+ * @head: pointer to the head of the list
+ */
+void free_listint(listint_t *head)
+{
+	while (head)
+	{
+		free(head);
+		head = head->next;
+	}
+}
