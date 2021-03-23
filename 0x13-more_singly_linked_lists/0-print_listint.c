c25711376808e18a75ec7a7595a6e9ad0530e4a1
vanemcb <2970@holbertonschool.com>
2021-03-23 02:34:45 +0000

    Function print_listint created

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_listint - function that prints all the elements of a list
+ * @h: pointer to the head of the list
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	unsigned int size;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		size++;
+	}
+	return (size);
+}
