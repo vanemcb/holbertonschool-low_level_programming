c3759fd7c455aa0b10f3334cc9766be675f1ec3d
vanemcb <2970@holbertonschool.com>
2021-03-24 19:10:18 +0000

    Task 9 fixed

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,6 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new_node;
 		new_node->n = n;
 		new_node->next = NULL;
+		return (new_node);
 	}
 	else
 	{
