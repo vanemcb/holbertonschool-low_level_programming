83f1a1889171f6de4992547720221e7f1c15f292
vanemcb <2970@holbertonschool.com>
2021-03-24 18:08:28 +0000

    Function get_nodeint_at_index fixed

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -8,21 +8,24 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int x;
-	listint_t *new_node = malloc(sizeof(listint_t));
-
-	if (!new_node)
-		return (NULL);
+	unsigned int x = 0;
+	listint_t *i_node = head;
 
 	if (!head)
 		return (NULL);
 
-	new_node->next = head;
-	for (x = 0; x <= index; x++)
+	while (i_node)
 	{
-		new_node = new_node->next;
-		if (!new_node)
-			return (NULL);
+		i_node = i_node->next;
+		x++;
 	}
-	return (new_node);
+
+	if (x < index)
+		return (NULL);
+
+	i_node = head;
+	for (x = 0; x < index; x++)
+		i_node = i_node->next;
+
+	return (i_node);
 }
