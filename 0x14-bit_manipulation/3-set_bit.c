c86927ad96dda464fd6ca64f61af7a360d28f43d
vanemcb <vanem2201@gmail.com>
2021-03-26 14:26:45 -0500

    Modified function structure

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,39 +9,13 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int n2, decimal = 0, n3;
-	unsigned int x = 0, x2, i;
-	int bit, bin[256];
+	unsigned long int one;
 
-	n3 = *n;
-	if (*n == 0)
-	{
-		for (i = 0; i <= index; i++)
-		{
-			bin[i] = 0;
-			x++;
-		}
-	}
-
-	while (*n)
-	{
-		n2 = *n;
-		*n = *n >> 1;
-		bit = n2 - (*n * 2);
-		bin[x] = bit;
-		x++;
-	}
-
-	if (index > (x - 1))
-	{
-		*n = n3;
+	if (index > 63)
 		return (-1);
-	}
 
-	bin[index] = 1;
+	one = 1 << index;
+	*n = *n | one;
 
-	for (x2 = 0; x2 < x; x2++)
-		decimal += bin[x2] << x2;
-	*n = decimal;
 	return (1);
 }
