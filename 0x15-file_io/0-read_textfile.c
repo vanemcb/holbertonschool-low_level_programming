955666bd874d9eed7df603778ae9e9f7e28706a3
vanemcb <vanem2201@gmail.com>
2021-03-29 14:50:51 -0500

    Functin read_textfile created

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * function that reads a text file and prints it to the POSIX
+ * standard output
+ * @filename: file name
+ * @letters: number of letters it should read and print
+ * Return: number of letters or 0 if it fail
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer = malloc((sizeof(char) * letters) + 1);
+	int fd, sz;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd < 0)
+		return (0);
+
+	sz = read(fd, buffer, letters + 1);
+
+	write(1, buffer, letters);
+
+	close(fd);
+
+	return (sz);
+}
---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,9 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif
