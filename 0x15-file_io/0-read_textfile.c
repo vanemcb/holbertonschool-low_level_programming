ab2b84dc1caef2faeaa1d8fa7db4fcc834534e22
vanemcb <vanem2201@gmail.com>
2021-03-29 14:52:42 -0500

    Betty style fixed

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * function that reads a text file and prints it to the POSIX
+ * read_textfile - function that reads a text file and prints it to the POSIX
  * standard output
  * @filename: file name
  * @letters: number of letters it should read and print
