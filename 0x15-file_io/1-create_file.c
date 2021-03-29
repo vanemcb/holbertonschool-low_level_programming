0afa30d48b821ad4a57a63cc09ae2bfd80814767
vanemcb <vanem2201@gmail.com>
2021-03-29 18:03:09 -0500

    Write size fixed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,6 @@ int create_file(const char *filename, char *text_content)
 
 	while (text_content[i] != '\0')
 		i++;
-	printf("%d", i);
 
 	bytes_write = write(fd, text_content, i);
 	if (bytes_write == -1)
