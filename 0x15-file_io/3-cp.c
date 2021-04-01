b21d90fcd3307d8392e502f7b086f9df34af487a
vanemcb <vanem2201@gmail.com>
2021-04-01 12:34:27 -0500

    Loop to read and write added

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,7 +11,7 @@
 int main(int argc, char *argv[])
 {
 	char *file_from, *file_to, buf[1024];
-	int fd_from, fd_to, rd_from, wr_to;
+	int fd_from, fd_to, rd_from = 1024, wr_to;
 
 	if (argc != 3)
 	{
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 	file_from = argv[1];
 	file_to = argv[2];
 
-		fd_from = open(file_from, O_RDONLY);
+	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
 		e98(file_from);
 
@@ -30,13 +30,16 @@ int main(int argc, char *argv[])
 	if (fd_from == -1)
 		e99(file_to);
 
-	rd_from = read(fd_from, buf, 1024);
-	if (rd_from == -1)
-		e98(file_from);
+	while (rd_from == 1024)
+	{
+		rd_from = read(fd_from, buf, 1024);
+		if (rd_from == -1)
+			e98(file_from);
 
-	wr_to = write(fd_to, buf, rd_from);
-	if (wr_to == -1)
-		e99(file_to);
+		wr_to = write(fd_to, buf, rd_from);
+		if (wr_to == -1)
+			e99(file_to);
+	}
 
 	if (close(fd_from) == -1)
 		e100(fd_from);
