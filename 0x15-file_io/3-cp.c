b1214a4a3816e1d0ac60a7d21e66a671f9a9a0bd
vanemcb <vanem2201@gmail.com>
2021-04-01 12:57:21 -0500

    Open permissions fixed

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
 	if (fd_from == -1)
 		e98(file_from);
 
-	fd_to = open(file_to, O_RDWR | O_CREAT | O_TRUNC, 0664);
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_from == -1)
 		e99(file_to);
 
