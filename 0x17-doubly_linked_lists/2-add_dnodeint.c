95a282509dfbf5427abcbe7cff9c324d7deaa3a4
vanemcb <vanem2201@gmail.com>
2021-04-30 12:18:04 -0500

    Function add_dnodeint_end created

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -3,13 +3,15 @@
 /**
  * add_dnodeint - function that adds a new node at the beginning of a doubly
  * linked kist
- * @h: double pointer to head doubly linked list
+ * @head: double pointer to head doubly linked list
  * Return: the address of the new element, or NULL if it failed
+ * @n: element to add.
  */
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
 	if (!new_node)
 		return (NULL);
 
---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - function that adds a new node at the end of a doubly
+ * linked kist
+ * @head: double pointer to head doubly linked list
+ * @n: element to add
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *aux_head = *head, *new_node = malloc(sizeof(dlistint_t));
+
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (!*head)
+		*head = new_node;
+	else
+	{
+		while (aux_head->next)
+			aux_head = aux_head->next;
+		aux_head->next = new_node;
+	}
+	return (*head);
+}
