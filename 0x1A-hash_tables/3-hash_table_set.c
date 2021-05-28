da5d9e52dee593897e3b42ddfbbcb8a820faf7e6
vanemcb <vanem2201@gmail.com>
2021-05-28 10:14:52 -0500

    Function hash_table_set created

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - function that adds an element to the hash table.
+ * @ht: the hash table you want to add or update the key/value to.
+ * @key: the key.
+ * @value: the value associated with the key.
+ * Return: if it succeeded, 0 otherwise.
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node = NULL;
+
+	if (!key)
+		return (0);
+
+	index = key_index((unsigned const char *)key, ht->size);
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
+}
