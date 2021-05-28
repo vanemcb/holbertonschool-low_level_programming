a2cf1b6abb97ee02d0c90621c6be4f67c5360bb9
vanemcb <vanem2201@gmail.com>
2021-05-28 16:40:37 -0500

    Maldito valgrindgit add 3-hash_table_set.c 6-hash_table_delete.c git add 3-hash_table_set.c 6-hash_table_delete.c !

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -23,6 +23,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(head->key, key) == 0)
 		{
+			free(head->value);
 			head->value = strdup(value);
 			return (1);
 		}
