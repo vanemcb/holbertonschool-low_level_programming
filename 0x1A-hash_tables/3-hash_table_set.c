cb6af3bd2fe2195ca61afcc25ea37bc60848043d
vanemcb <vanem2201@gmail.com>
2021-05-28 14:12:03 -0500

    Condition added

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new_node = NULL;
 
-	if (strcmp(key, "") == 0 || !key || !ht || !ht->array)
+	if (strcmp(key, "") == 0 || !key || !ht || !value)
 		return (0);
 
 	index = key_index((unsigned const char *)key, ht->size);
