3ea30dcf21f0b69728eda413312d4553d1c3f71a
vanemcb <vanem2201@gmail.com>
2021-05-28 13:55:21 -0500

    Function hash_table_print created

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function that prints a hash table.
+ * @ht: he hash table.
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int index = 0;
+
+	printf("{");
+	while (index < ht->size)
+	{
+		while (ht->array[index])
+		{
+			printf("'%s': '%s'", ht->array[index]->key, ht->array[index]->value);
+			printf(", ");
+			ht->array[index]= ht->array[index]->next;
+		}
+		index++;
+	}
+	printf("}\n");
+}
