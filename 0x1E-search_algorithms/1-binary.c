f8a7b1f1d5a927826cbd7ae9bc4bbbc22a5b05e0
vanemcb <vanem2201@gmail.com>
2021-08-31 19:22:58 -0500

    test Julian code

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,44 +1,68 @@
 #include "search_algos.h"
 
 /**
- * binary_search - function that searches for a value in a sorted array of
+ * print_array - prints an array from pos floor to top
+ * @array: is a pointer to the first element of the array to search in
+ * @floor: starting printing position
+ * @top: ending printing position
+ */
+
+void print_array(int *array, size_t floor, size_t top)
+{
+	size_t i = floor;
+
+	printf("Searching in array: ");
+	while (i <= top)
+	{
+		printf("%d", array[i]);
+		if (i != top)
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+}
+
+/**
+ * binary_search - searches for a value in an array of
  * integers using the Binary search algorithm
- * @array: pointer to the first element of the array to search in
- * @size: the number of elements in array
- * @value: the value to search for
- * Return: the first index where value is located
+ * @array: is a pointer to the first element of the array to search in
+ * @size: is the number of elements in array
+ * @value: is the value to search for
+ * Return: first index where value is found or -1 if
+ * not found or if array is Null
  */
 
 int binary_search(int *array, size_t size, int value)
 {
-	size_t i, low, high, mid;
+	size_t floor, top, middle;
 
-	if (!array)
-		return (-1);
-
-	low = 0;
-	high = size - 1;
-
-	while (low <= high)
+	if (array != NULL && size > 0)
 	{
-		mid = (low + high) / 2;
-
-		printf("Searching in array: ");
-
-		for (i = low; i <= high; i++)
+		floor = 0;
+		top = size - 1;
+		print_array(array, floor, top);
+		while (floor < top)
 		{
-			printf("%d", array[i]);
-			if (i != high)
-				printf(", ");
+			middle = (floor + top) / 2;
+			/**
+			 * If value greater, ignore left half
+			*/
+			if (array[middle] < value)
+				floor = middle + 1;
+			/**
+			 *  If value is smaller, ignore right half
+			*/
+			else if (array[middle] > value)
+				top = middle - 1;
+			/**
+			 *  Check if value is present at mid
+			*/
+			else
+			{
+				return (middle);
+			}
+			print_array(array, floor, top);
 		}
-		printf("\n");
-
-		if (value == array[mid])
-			return (mid);
-		else if (value > array[mid])
-			low = mid + 1;
-		else
-			high = mid - 1;
 	}
 	return (-1);
 }
